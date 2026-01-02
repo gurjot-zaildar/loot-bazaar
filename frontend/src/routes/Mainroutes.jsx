0c34abd2bd5a0296a9de59cb5a9ba645e49e49c4
gurjot-zaildar <signingames115@gmail.com>
2026-01-02 12:36:16 +0530

    authwrapper

--- frontend/src/routes/Mainroutes.jsx
+++ frontend/src/routes/Mainroutes.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/user/Home'
 import Products from '../pages/user/Products'
 import Login from '../pages/user/Login'
 import Register from '../pages/user/Register'
 import CreateProduct from '../pages/admin/CreateProduct'
 import ProductDetail from '../pages/admin/ProductDetail'
-import { useSelector } from 'react-redux'
 import UserProfile from '../pages/user/UserProfile'
 import PageNotFound from '../pages/user/PageNotFound'
+import AuthWrapper from './AuthWrapper'
 
 const Mainroutes = () => {
-  const {users}= useSelector((state)=> state.userReducer);
   return (
     <div>
      <Routes>
@@ -19,10 +16,13 @@ const Mainroutes = () => {
         <Route path='/login' element={<Login/>} />
         <Route path='/register' element={<Register/>} />
 
+        
+        
+        <Route path='/admin/create-product' element={<AuthWrapper><CreateProduct/></AuthWrapper>} />
+        <Route path='/admin/user-profile' element={<AuthWrapper><UserProfile/></AuthWrapper>} />
+        <Route path='/product/:id' element={<AuthWrapper><ProductDetail/></AuthWrapper>} />
+        
 
-        <Route path='/admin/create-product' element={<CreateProduct/>} />
-        <Route path='/admin/user-profile' element={<UserProfile/>} />
-        <Route path='/product/:id' element={<ProductDetail/>} />
         <Route path='*' element={<PageNotFound/>} />
         </Routes>       
     </div>
